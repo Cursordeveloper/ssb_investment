a9fb37eabec5742e8a712516159ad95bd5e1f072
Michael <billyston@Michaels-MacBook-Pro.local>
2023-11-25 15:28:33 +0000

    Packages updated.

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-/**
- * Version 1
- */
-Route::prefix('v1')
-    ->as('v1:')
-    ->middleware(['ip_address', 'allow:GET'])
+Route::prefix('v1/investment')
+    ->as('v1.investment:')
     ->group(base_path('routes/v1/routes.php'));
